02de24c9fd78d3bbd93fad161fef14ef64ceb57d
shahbaz faqir <shabaz.h000pm@gmail.com>
2024-03-18 13:01:34 +0500

    second comment

--- oop.py
+++ oop.py
@@ -2,7 +2,7 @@ class student:
     def __init__(self,name,age):
         self.name=name
         self.age=age
-        print("this is first project ")
+        print("this is first project")
 
 
 s1=student("ali",25)
