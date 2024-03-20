acb9213bb61766b124bf54b83c60dddca2168c68
shahbaz faqir <shabaz.h000pm@gmail.com>
2024-03-20 13:38:43 +0500

    making class and object

--- oop.py
+++ oop.py
@@ -1,9 +1,53 @@
-class student:
-    def __init__(self,name,age):
-        self.name=name
+# class student:
+#     def __init__(self,name,age):
+#         self.name=name
+#         self.age=age
+#         print("this is first project")
+
+
+# s1=student("ali",25)
+# print(s1.name,s1.age)
+
+
+#oop 
+class Student:
+
+    college="abc college"
+    print(college)
+
+    def __init__(self,name,age,department):
+        self.name= name
         self.age=age
-        print("this is first project")
+        self.department=department
+        print("IT department students")
+
+s1=Student("ali",25,"CS")
+print(s1.name,s1.age,s1.department)
+
+s2=Student("usman",24,"sw")
+print(s2.name,s2.age,s2.department)
 
 
-s1=student("ali",25)
-print(s1.name,s1.age)
+
+
+
+
+
+
+# class Car:
+#     color="black"
+#     brand="bmw"
+
+# s1=Car()
+# print(s1.color)
+# print(s1.brand)
+
+
+
+
+
+
+
+
+
+
