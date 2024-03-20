d3134d6c5419d7c6b59fb609726dfea891dd5c92
shahbaz faqir <shabaz.h000pm@gmail.com>
2024-03-20 14:32:24 +0500

    20th comment

---
+++ sets.py
@@ -0,0 +1,7 @@
+letters = {"a", "b", "c", "b"}  # Duplicates are automatically removed
+
+print(letters)  # Output: {'a', 'b', 'c'} (order may vary)
+
+# Adding elements to a set
+letters.add("d")
+print(letters)  # Output: {'a', 'b', 'c', 'd'}
