10bcb795b7397359b8fbad70f486fd7bd1af4c0d
shahbaz faqir <shabaz.h000pm@gmail.com>
2024-03-21 04:53:03 +0500

    21st comment

--- oop.py
+++ oop.py
@@ -10,34 +10,68 @@
 
 
 #oop 
-class Student:
+# class Student:
 
-    college="abc college"
-    print(college)
+#     college="abc college"
+#     print(college)
 
-    def __init__(self,name,age,department):
-        self.name= name
-        self.age=age
-        self.department=department
-        print("IT department students")
+#     def __init__(self,name,age,department):
+#         self.name= name
+#         self.age=age
+#         self.department=department
+#         print("IT department students")
 
-    def wellcome(self):
-        print("wellcome students",self.name)
+#     def wellcome(self):
+#         print("wellcome students",self.name)
     
-    def age(self):
-        return self.age
+#     def age(self):
+#         return self.age
 
-s1=Student("ali",25,"CS")
-print(s1.name,s1.age,s1.department)
+# s1=Student("ali",25,"CS")
+# print(s1.name,s1.age,s1.department)
 
-s2=Student("usman",24,"sw")
-print(s2.name,s2.age,s2.department)
+# s2=Student("usman",24,"sw")
+# print(s2.name,s2.age,s2.department)
 
 
-s1.wellcome()
-s2.wellcome()
+# s1.wellcome()
+# s2.wellcome()
 
-print(s1.age)
+# print(s1.age)
+
+
+
+
+class Student:
+    def __init__(self,name,math,oop,algo):
+
+        self.name=name
+        self.math=math
+        self.oop=oop
+        self.algo=algo
+
+        def avg_marks(self):
+            sub=math+oop+algo
+            sum1=sub/3
+
+            return sum1
+
+
+s1=Student("ali",70,80,90)
+
+
+print(sum1)
+
+
+
+
+
+        
+
+
+
+
+
 
 
 
